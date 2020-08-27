8412d559e8f826b56f5945ef7d750f6ff746d20e
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-27 12:56:07 -0500

    Updates

---
+++ precache-manifest.2e0c8f40a83aa6c1061a4c812ef95d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5ead4b725557faf38cb21b9fd3385a",
+    "url": "/imc-comfenalco/index.html"
+  },
+  {
+    "revision": "d77843e84ed320637ddf",
+    "url": "/imc-comfenalco/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e64b266f909918925f3a",
+    "url": "/imc-comfenalco/static/js/2.1a295d35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imc-comfenalco/static/js/2.1a295d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d77843e84ed320637ddf",
+    "url": "/imc-comfenalco/static/js/main.e8f13722.chunk.js"
+  },
+  {
+    "revision": "cf5ef7bb299543477c49",
+    "url": "/imc-comfenalco/static/js/runtime-main.e4874b84.js"
+  }
+]);
