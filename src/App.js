4ca39363968a1758089159e863ea1425343ace58
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-28 12:13:35 -0500

    cambios en proyeccion

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bulma/css/bulma.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import CalculadorApp from './components/imc/captura/CalculadorApp'
-
+import DetalleIMC from './components/imc/proyeccion/DetalleIMCApp'
 export default class App extends React.Component {
   componentWillMount(){
     this.setState({imc:0})
@@ -13,6 +13,7 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <CalculadorApp App={this}/>
+        <DetalleIMC App={this}/>
       </div>
     );
   }
